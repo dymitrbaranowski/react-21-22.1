663eacb7d820dfc4a5d0878c9415dbe28e6d3aaf
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-12-06 19:43:28 +0200

    fox1

--- src/index.js
+++ src/index.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+const element = React.createElement('div', {
+  a: 5,
+  b: 10,
+  children: 'Привет !',
+});
+
+console.log(element);
+
+// ReactDOM.render(element, document.getElementById('root'));
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.StrictMode>{<App />}</React.StrictMode>
 );
