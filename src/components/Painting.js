0a1bc1c4443264928812fcd0b15fb5f3307664b4
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-12-09 11:15:00 +0200

    fox1

---
+++ src/components/Painting.js
@@ -0,0 +1,22 @@
+export default function Painting({
+  url,
+  title,
+  profileUrl,
+  author,
+  price,
+  quantity,
+}) {
+  // const { url, title, profileUrl, author, price, quantity } = props;
+  return (
+    <div>
+      <img src={url} alt={title} width="480" />
+      <h2>{title}</h2>
+      <p>
+        Автор: <a href={profileUrl}>{author}</a>
+      </p>
+      <p>Цена: {price} кредитов</p>
+      <p>Доступность:{quantity < 10 ? 'заканчивается' : 'есть в наличии'}</p>
+      <button type="button">Добавить в корзину</button>
+    </div>
+  );
+}
