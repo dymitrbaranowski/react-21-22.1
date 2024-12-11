0e832166be59149ea6abbf76ab7d64e53bae7f96
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-12-11 20:03:23 +0200

    fox2

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,4 +1,10 @@
 import PropTypes from 'prop-types';
+import {
+  FaMapMarkerAlt,
+  FaUserAlt,
+  FaCalendarAlt,
+  FaClock,
+} from 'react-icons/fa';
 import css from './Event.module.css';
 
 export const Event = ({ name, location, speaker, type, start, end }) => {
@@ -6,19 +12,19 @@ export const Event = ({ name, location, speaker, type, start, end }) => {
     <div className={css.event}>
       <h2 className={css.title}>{name}</h2>
       <p className={css.info}>
-        <i className="icon"></i>
+        <FaMapMarkerAlt className={css.icon} size={16} />
         {location}
       </p>
       <p p className={css.info}>
-        <i className="icon"></i>
+        <FaUserAlt className={css.icon} size={16} />
         {speaker}
       </p>
       <p className={css.info}>
-        <i className="icon"></i>
+        <FaCalendarAlt className={css.icon} size={16} />
         {start}
       </p>
       <p className={css.info}>
-        <i className="icon"></i>
+        <FaClock className={css.icon} size={16} />
         Duration
       </p>
     </div>
@@ -29,7 +35,7 @@ Event.propTypes = {
   name: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   speaker: PropTypes.string.isRequired,
-  type: PropTypes.bool.isRequired,
+  type: PropTypes.string.isRequired,
   start: PropTypes.string.isRequired,
   end: PropTypes.string.isRequired,
 };
