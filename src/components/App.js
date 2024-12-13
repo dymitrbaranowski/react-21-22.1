f46230d5bd05122d889b85ac4c1c584b9084ba0e
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-12-13 12:15:53 +0200

    fox1

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
-import Counter from 'components/Counter';
-
+//import Counter from 'components/Counter';
+import Dropdown from 'components/Dropdown';
 export const App = () => {
   return (
     <div>
       <h1>Состояние компонента</h1>
-      <Counter />
+      {/* <Counter initialValue={0} /> */}
+      <Dropdown />
     </div>
   );
 };
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,30 +1,40 @@
 import React from 'react';
+import Controls from './Controls';
+import Value from './Value';
 import './Counter.css';
 
 class Counter extends React.Component {
+  static defaultProps = {
+    initialValue: 0,
+  };
+
+  static propTypes = {
+    //
+  };
+  state = {
+    value: this.props.initialValue,
+  };
+
+  handleIncrement = () => {
+    this.setState(prevState => ({
+      value: prevState.value + 1,
+    }));
+  };
+
+  handleDecrement = () => {
+    this.setState(prevState => ({
+      value: prevState.value - 1,
+    }));
+  };
+
   render() {
     return (
       <div className="Counter">
-        <span className="Counter__value">0</span>
-
-        <div className="Counter__controls">
-          <button
-            type="button"
-            onClick={() => {
-              console.log('Кликнули в увеличить');
-            }}
-          >
-            Увеличить на 1
-          </button>
-          <button
-            type="button"
-            onClick={() => {
-              console.log('Кликнули на уменьшить');
-            }}
-          >
-            Уменьшить на 1
-          </button>
-        </div>
+        <Value value={this.state.value} />
+        <Controls
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+        />
       </div>
     );
   }
