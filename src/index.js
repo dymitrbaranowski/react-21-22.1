ecd0f2c4a8d1e9a141f6df43b12ad49a27098d56
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-01-14 18:06:20 +0200

    fox2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'components/GlobalStyle';
-// import { Example1 } from 'components/Example1';
-import { Reader } from 'components/Reader/Reader';
+import { Example1 } from 'components/Example1';
+//import { Reader } from 'components/Reader/Reader';
+
 import './index.css';
-import publications from './publications.json';
+//import publications from './publications.json';
 
 const theme = {};
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Reader items={publications} />
+      <Example1 />
+      {/* <Reader items={publications} /> */}
     </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
