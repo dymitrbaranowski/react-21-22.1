fdc686bf6427900b5041d9da60fa09d1df60bf61
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-01-19 19:29:10 +0200

    fox1

--- src/components/MaterialList/MaterialList.jsx
+++ src/components/MaterialList/MaterialList.jsx
@@ -1,27 +1,11 @@
-//import { Material } from '../Material/Material';
+import { Material } from '../Material/Material';
 
-export const MateriaList = ({ items, onDelete, onUpdate }) => {
+export const MateriaList = ({ items, ...otherProps }) => {
   return (
     <ul>
       {items.map(item => (
         <li key={item.id}>
-          <p>
-            <b>Название: </b>
-            {item.title}
-          </p>
-          <p>
-            <b>Ссылка: </b> {item.link}
-          </p>
-          <button type="button" onClick={() => onDelete(item.id)}>
-            Удалить
-          </button>
-
-          <button
-            type="button"
-            onClick={() => onUpdate({ id: item.id, title: Date.now() })}
-          >
-            Редактировать
-          </button>
+          <Material item={item} {...otherProps} />
           <hr />
         </li>
       ))}
