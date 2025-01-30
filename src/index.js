43c754b8a436d2ebdbc0252d1662297265923a61
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-01-30 19:41:24 +0200

    fox1

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  
-} from "react-router-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { createBrowserRouter } from 'react-router-dom';
 import App from './App';
 import AuthProvider from './contexts/auth/Provider';
 import 'modern-normalize/modern-normalize.css';
@@ -15,4 +11,25 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);
+
+//-------------------------------------------------
+
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import AuthProvider from './contexts/auth/Provider';
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
